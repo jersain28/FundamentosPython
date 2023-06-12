2b003a0df85677fef1e67669e8fccb1f32ebdb2e
Jersain Marin <jersainmarin20679@gmail.com>
2023-06-12 09:53:50 -0600

    05 Operadores Logicos y Booleanos

---
+++ operadores2.py
@@ -0,0 +1,22 @@
+#Operadores booleanos
+#> >=
+print(10 > 10)
+print(10 >= 10)
+#< <=
+print(10 < 10)
+print(10 <= 10)
+#==
+print(10 == 10)
+#!=
+print(10 != 10)
+
+#Operadores Logicos
+#and (True if all true)
+print(True and True)
+print(True and False)
+#or (True if al least one true)
+print(True or True)
+print(True or False)
+#not (not true)
+print(not True)
+print(not False)
