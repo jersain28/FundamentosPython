9b10da5919b4de3b885a1f426d61d3ca50662c29
Jersain Marin <jersainmarin20679@gmail.com>
2023-06-13 14:58:07 -0600

    08 Listas

---
+++ colecciones1.py
@@ -0,0 +1,21 @@
+#Listas
+#Entre corches []
+my_list=[123,True,3.14,"Hi!"]
+print(type(my_list))
+print(my_list)
+
+#Indexing
+print(my_list[0])
+#Slicing
+print(my_list[2:])
+
+students=["Paquiño","Lalin","Kikin"]
+print(students)
+students.append("Paquiño")
+students.append("Lalin")
+students.append("Kikin")
+print(students)
+students.remove("Paquiño")
+print(students)
+students[1]*"Vero"
+print(students)
