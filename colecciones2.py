5e9ef9f60f04442a0dfe5aef2472bfbcde6d5938
Jersain Marin <jersainmarin20679@gmail.com>
2023-06-14 11:27:39 -0600

    09 Truplas y Conjuntos

---
+++ colecciones2.py
@@ -0,0 +1,16 @@
+#Truplas
+my_truple=('Hola',12,1.5,True)
+print(type(my_truple))
+print(my_truple[0])
+print(my_truple[-1])
+#Eroro
+#my_truple[0]="Otro valor"
+
+#Conjuntos (set)
+my_set={1,2,4,4,4,4,True,'hi'}
+print(type(my_set))
+print(my_set)
+
+lista=[1,2,3,1,2,4,3,5,6]
+print(lista)
+print(set(lista))
